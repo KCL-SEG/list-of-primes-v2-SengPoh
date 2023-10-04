1bbb875cb47f6f8cd63af1aa067a5cb2b13bd147
k22002448 <k22002448@kcl.ac.uk>
2023-10-04 19:16:35 +0100

    complete primes function

--- primes.py
+++ primes.py
@@ -3,4 +3,21 @@
 
 def primes(number_of_primes):
     list = []
+
+    currentNum = 2
+    for i in range(number_of_primes):
+        while True:
+            isPrime = True
+            for n in range(2, currentNum):
+                #if number is divisible
+                if (currentNum % n == 0):
+                    currentNum = currentNum + 1
+                    isPrime = False
+                    break
+            
+            if (isPrime):
+                list.append(currentNum)
+                currentNum = currentNum + 1
+                break
+
     return list
